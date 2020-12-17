08ac0abe29803ad3946fd98dbb186abd3189bc34
dibyajit <dibyajit30@gmail.com>
2020-12-16 19:51:19 -0500

    Visualize

---
+++ util.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+import os
+import pickle
+
+import numpy as np
+import torch
+from torch.utils.data.sampler import Sampler
+
+import torchvision.models as models
+
+def load_model(path):
+    """Loads model and return it without DataParallel table."""
+    if os.path.isfile(path):
+        print("=> loading checkpoint '{}'".format(path))
+        checkpoint = torch.load(path)
+
+        # size of the top layer
+        N = checkpoint['state_dict']['top_layer.bias'].size()
+
+        # build skeleton of the model
+        sob = 'sobel.0.weight' in checkpoint['state_dict'].keys()
+        #model = models.alexnet(pretrained=False)
+        model = models.__dict__[checkpoint['arch']](sobel=sob, out=int(N[0]))
+
+        # deal with a dataparallel table
+        def rename_key(key):
+            if not 'module' in key:
+                return key
+            return ''.join(key.split('.module'))
+
+        checkpoint['state_dict'] = {rename_key(key): val
+                                    for key, val
+                                    in checkpoint['state_dict'].items()}
+
+        # load weights
+        model.load_state_dict(checkpoint['state_dict'])
+        print("Loaded")
+    else:
+        model = None
+        print("=> no checkpoint found at '{}'".format(path))
+    return model
